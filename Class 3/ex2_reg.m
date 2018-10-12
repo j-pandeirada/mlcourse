ea65d9990a68b5ce4635d894ffbf9a3cd4706d25
unknown <joao.pandeirada@ua.pt>
2018-10-12 14:31:04 +0100

    logistic regr. with reg. finished, predicts succesfully and shows how the hypothesis varies with lambda

--- Class 3/ex2_reg.m
+++ Class 3/ex2_reg.m
@@ -10,18 +10,19 @@ clear ; close all; clc
 %% Step 1: Load and Plot Data from file ex2data2.txt
 %  The first two columns contains the X values and the third column
 %  contains the label (y).
-load
-X = 
-y = 
+load ex2data2.txt
+
+X = ex2data2(:,1:2);
+y = ex2data2(:,3);
 
 plotData(X, y);
 
 % Put labels and Legend
-xlabel
-ylabel
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
 
 % Specified in plot order
-legend
+legend('y=1','y=0')
 hold off;
 
 %% =========== Part 1: Regularized Logistic Regression ============
@@ -35,8 +36,11 @@ hold off;
 % Note that mapFeature also adds a column of ones, so the intercept term is handled
 X = mapFeature(X(:,1), X(:,2)); 
 
+%  Data matrix dimension
+[m, n] = size(X);
+
 % Initialize fitting parameters =0
-initial_theta =
+initial_theta = zeros(n,1);
 
 % Set regularization parameter lambda
 lambda = 1;
@@ -62,39 +66,47 @@ fprintf('Cost at initial theta (zeros): %f\n', cost);
 initial_theta = zeros(size(X, 2), 1);
 
 % Set regularization parameter lambda (you should vary this)
+lambda = [0 1 10 100];
+
+% Set Options
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+for i=1:length(lambda),
+    % Optimize
+    [theta, J, exit_flag] = ...
+        fminunc(@(t)(costFunctionReg(t, X, y, lambda(i))), initial_theta, options);
+
+    % Plot Boundary
+    plotDecisionBoundary(theta, X, y);
+    hold on;
+    title(sprintf('lambda = %g', lambda(i)))
+
+    % Labels and Legend
+    xlabel('Microchip Test 1')
+    ylabel('Microchip Test 2')
+
+    legend('y = 1', 'y = 0', 'Decision boundary')
+    hold off;
+
+    % Compute accuracy on our training set
+    p = predict(theta, X);
+    fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
+end
+
+%% Using proper lambda and predicting
+
+% Set regularization parameter lambda (you should vary this)
 lambda = 1;
 
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
-
 % Optimize
-[theta, J, exit_flag] = ...
-	fminunc(@(t)(costFunctionReg(t, X, y, lambda)), initial_theta, options);
-
-% Plot Boundary
-plotDecisionBoundary(theta, X, y);
-hold on;
-title(sprintf('lambda = %g', lambda))
-
-% Labels and Legend
-xlabel('Microchip Test 1')
-ylabel('Microchip Test 2')
-
-legend('y = 1', 'y = 0', 'Decision boundary')
-hold off;
-
-% Compute accuracy on our training set
-p = predict(theta, X);
-fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
-
-
-Acc_train=mean(double(p == y)) * 100;
-fprintf('Train Accuracy: %f\n', Acc_train);
-
+    [theta, J, exit_flag] = ...
+        fminunc(@(t)(costFunctionReg(t, X, y, lambda)), initial_theta, options);
 
 %predict a new example: Xtest=(-0.25, 1.5), y=0 (rejected)
 %Do not forget to apply first the mapFeature function
-
-p_test=
+Xtest = [-0.25 1.5];
+Xtest = mapFeature(Xtest(:,1), Xtest(:,2)); 
+p_test= predict(theta,Xtest)
 
 
